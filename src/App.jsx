51eb94786552b2aafecf5ec3e632d1121fd2d103
mennaelammal-prog <mennaelammal@gmail.com>
2026-07-29 23:34:42 +0800

    Switch to HashRouter for static hosting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 ﻿import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import Login from './components/Login';
@@ -9,7 +9,7 @@ import Dashboard from './components/Dashboard';
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
@@ -22,7 +22,7 @@ function App() {
             }
           />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </AuthProvider>
   );
 }
